4ef571e79deeecc0be2ea871fb25d192c7a298c1
sharifabee <sharifabee.alur@gmail.com>
2025-04-28 15:39:54 +0530

    table

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ListDemo from './varun-components/ListDemo'
+import TableDemo from './varun-components/TableDemo'
 
 export default function App() {
   return (
     <div>
 
-<ListDemo />
+<TableDemo />
 
 
 
