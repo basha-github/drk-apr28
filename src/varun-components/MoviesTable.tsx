5959276832199c907992676d88bc6cf06a238075
sharifabee <sharifabee.alur@gmail.com>
2025-04-28 16:01:46 +0530

    table-movies

--- src/varun-components/MoviesTable.tsx
+++ src/varun-components/MoviesTable.tsx
@@ -1,76 +1,66 @@
-import React from 'react'
+import React from "react";
 
 export default function MoviesTable() {
-
-const movies = [
+  const movies = [
     {
-      "Title": "The Lion King",
-      "Year": "2019",
-      "Runtime": "118 min",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjIwMjE1Nzc4NV5BMl5BanBnXkFtZTgwNDg4OTA1NzM@._V1_SX300.jpg"
+      Title: "The Lion King",
+      Year: "2019",
+      Runtime: "118 min",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjIwMjE1Nzc4NV5BMl5BanBnXkFtZTgwNDg4OTA1NzM@._V1_SX300.jpg",
     },
     {
-      "Title": "Mowgli: Legend of the Jungle",
-      "Year": "2018",
-      "Runtime": "104 min",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjMzODc2NzU5MV5BMl5BanBnXkFtZTgwNTMwMTE3NjM@._V1_SX300.jpg"
+      Title: "Mowgli: Legend of the Jungle",
+      Year: "2018",
+      Runtime: "104 min",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjMzODc2NzU5MV5BMl5BanBnXkFtZTgwNTMwMTE3NjM@._V1_SX300.jpg",
     },
     {
-      "Title": "Doctor Strange",
-      "Year": "2016",
-      "Runtime": "115 min",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNjgwNzAzNjk1Nl5BMl5BanBnXkFtZTgwMzQ2NjI1OTE@._V1_SX300.jpg"
+      Title: "Doctor Strange",
+      Year: "2016",
+      Runtime: "115 min",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNjgwNzAzNjk1Nl5BMl5BanBnXkFtZTgwMzQ2NjI1OTE@._V1_SX300.jpg",
     },
     {
-      "Title": "John Wick",
-      "Year": "2014",
-      "Runtime": "101 min",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300.jpg"
+      Title: "John Wick",
+      Year: "2014",
+      Runtime: "101 min",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300.jpg",
     },
     {
-      "Title": "The Notebook",
-      "Year": "2004",
-      "Runtime": "123 min"
-    }
+      Title: "The Notebook",
+      Year: "2004",
+      Runtime: "123 min",
+    },
   ];
-
-
 
   return (
     <div>
-
-<table className='table'>
-
-<thead>
-    <tr>
-        <th>Title</th>
-        <th>Runtime</th>
-        <th>Year</th>
-        <th>Poster</th>
-    </tr>
-</thead>
-<tbody>
-{movies.map(
-    (movie)=>(
-        <div>
-<tr>
-            <td>{movie.Title}</td>
-            <td>{movie.Runtime}</td>
-            <td>{movie.Year}</td>
-            <td><img src={movie.Poster} /></td>
-        </tr>
-        <br/>
-        </div>
-        
-    )
-)}
-
-</tbody>
-
-</table>
-
-
-
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Runtime</th>
+            <th>Year</th>
+            <th>Poster</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((movie) => (
+              <tr>
+                <td>{movie.Title}</td>
+                <td>{movie.Runtime}</td>
+                <td>{movie.Year}</td>
+                <td>
+                  <img src={movie.Poster} />
+                </td>
+              </tr>
+                      ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
