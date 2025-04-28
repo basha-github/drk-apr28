54c43c9b9cfa24875867bc5e7144270a2410ef0d
sharifabee <sharifabee.alur@gmail.com>
2025-04-28 15:50:32 +0530

    table-movies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ListDemo from './varun-components/ListDemo'
 import TableDemo from './varun-components/TableDemo'
+import MoviesTable from './varun-components/MoviesTable'
 
 export default function App() {
   return (
     <div>
 
-<TableDemo />
+<MoviesTable />
 
 
 
